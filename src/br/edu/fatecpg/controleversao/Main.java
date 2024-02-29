bb7d6f6d72add02d7e8781d26c074b33ebd93f43
Re Gomes <renatogomes1616@gmail.com>
2024-02-29 16:18:34 -0300

    Criação de projeto java

---
+++ src/br/edu/fatecpg/controleversao/Main.java
@@ -0,0 +1,11 @@
+package br.edu.fatecpg.controleversao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
